8e8a63966bdc12821ab7fbedb77c41345be1761a
DESKTOP-TB6T7QS\Raquel Bernal <rbernal26@misena.edu.co>
2021-10-28 13:46:49 -0500

    23 09 2021 se modificn los controladores y se crea usuarios html y js

---
+++ 20092021b/Pagina/usuarios.js
@@ -0,0 +1,13 @@
+
+
+var Guardar = function () {
+//capturar datos de entrada
+    var nombre = document.getElementById('nombre').value;
+    var apellidos = document.getElementById('apellidos').value;
+    var direccion = document.getElementById('direccion').value;
+
+    console.log(nombre)
+    console.log(apellidos)
+    console.log(direccion)
+    
+}
